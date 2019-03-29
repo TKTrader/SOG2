fe76e887b73a18d09991be50b84cf068a4acd130
Daniel_S <dfsimeon@gmail.com>
2019-03-28 22:23:32 -0400

    Refactoring

---
+++ schedulePage.php
@@ -0,0 +1,4 @@
+<?php
+require 'header.php';
+?>
+Here we need to create a sort of "calendar" like view but smaller, Olympics only last 16 Days. Similar to http://www.espn.com/olympics/summer/2016/schedule/_/date/20160817
