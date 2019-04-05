781d40736b40a1bffe963120c542c30632902e69
Sammy Samkough <sammysamkough@gmail.com>
2019-04-05 00:32:25 -0400

    added tickets page with comments on ideas

---
+++ tickets.php
@@ -0,0 +1,25 @@
+<?php
+require 'header.php';
+
+if ($access == 'A'){
+    /* request for free tickets */
+}
+
+if ($access == 'E'){
+    /* request for free tickets */
+}
+?>
+
+/*
+page displays cards of competition events with a buy ticket button
+    if user is logged in it shows that
+    if a user is not logged in it shows that but also a "sign in/sign up" link on the bottom
+*/
+
+/*
+tickets are per competition event
+tickets are available to public at full price
+athletes and employees request for free
+if competition event time changes, corresponding tickets are udpated
+medal awards ceremonies are free to go to 
+*/
