14e53a7326034ddda0b0b765ca14bdc87f24e8fc
Sammy Samkough <sammysamkough@gmail.com>
2019-04-08 00:47:18 -0400

    Added some stuff to ticket

--- tickets.php
+++ tickets.php
@@ -1,25 +1,39 @@
 <?php
 require 'header.php';
 
-if ($access == 'A'){
+/* IS THIS THE CORRECT SYNTAX TO SHOW DIFFERENT LOGINS? */
+if ($_SESSION['access'] == 'A'){
     /* request for free tickets */
-}
-
-if ($access == 'E'){
-    /* request for free tickets */
+    //header("Location: ./AthleteInterface/athleteIndex.php");
+    exit();
+} else if ($_SESSION['access'] == 'E'){
+     /* request for free tickets */
+    //header("Location: ./EmployeeInterface/employeeIndex.php");
+    exit();
+} else if ($_SESSION['access'] == 'P'){
+    //header("Location: ./PublicInterface/publicIndex.php");
+    exit();
+} else {
+    //header("location: Controllers/error.php");
 }
 ?>
 
-/*
+<html>
+    <body>
+        <?php require 'ticketCard.php' ?>
+    </body>
+</html>
+
+<!--
 page displays cards of competition events with a buy ticket button
     if user is logged in it shows that
     if a user is not logged in it shows that but also a "sign in/sign up" link on the bottom
-*/
+-->
 
-/*
+<!--
 tickets are per competition event
 tickets are available to public at full price
 athletes and employees request for free
 if competition event time changes, corresponding tickets are udpated
 medal awards ceremonies are free to go to 
-*/
+-->
