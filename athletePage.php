a388c9ebadf9a870c1c9738a3afd4081a31b9770
Tai-Yin Chong <t.m.chong16@gmail.com>
2019-04-09 17:09:12 -0400

    Add files via upload

--- athletePage.php
+++ athletePage.php
@@ -15,8 +15,30 @@ require "header.php";
       <a class="nav-item nav-link active" href="athletePage.php">Athletes</a>
       <a class="nav-item nav-link" href="registerPage.php">Register</a>
       <a class="nav-item nav-link" href="loginPage.php">Login</a>
-      <a class="nav-item nav-link" href="logout.php"> Logout</a></li>
+      <a class="nav-item nav-link" href="logout.php"> Logout</a>
     </div>
   </div>
 </nav>
 
+<div class="athletePage_container">
+    <hr>
+    <?php
+    //for now only name and country, could do sports later
+    $query1 = "SELECT * FROM athletes INNER JOIN users ON athletes.id=users.id";
+    echo "<table class=\"athletes\">";
+    $query1_result = mysqli_query($mysqli, $query1);
+    $counter = 1;
+    echo"<tr>";
+    while($row = mysqli_fetch_array($query1_result)) {
+        echo "<td><h4>" . $row['firstName'] . " " . $row['lastName'] . "</h3><ul><li>" . $row['country'] . "</li><li>" .
+            $row['DOB'] . "</li><li>" . $row['height'] . "</li><li>" . $row['wgt']  . "</ul></td>";
+        if($counter % 3 == 0) {
+            echo"</tr>";
+            echo"<tr>";
+        }
+        $counter++;
+    }
+    echo"</tr>";
+    echo "</table>"
+    ?>
+</div>
