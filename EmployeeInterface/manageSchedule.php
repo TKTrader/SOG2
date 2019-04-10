192d5c91ad016e124616b14bf19337cca00b07c5
Daniel_S <dfsimeon@gmail.com>
2019-04-10 05:09:45 -0400

    Insert query created and tested, works!

--- EmployeeInterface/manageSchedule.php
+++ EmployeeInterface/manageSchedule.php
@@ -7,6 +7,30 @@ if ($access != 'E') {
     $_SESSION['message'] = 'Invalid Access';
     header("location: error.php");
 }
+
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+  if (isset($_POST['AddtoSchedule_button'])) {
+
+    //Storing all posted inputs into php vars
+      $event_SELECTED = mysqli_real_escape_string($mysqli, $_POST['event']);
+      $category_SELECTED = mysqli_real_escape_string($mysqli, $_POST['category']);
+      $date_SELECTED = mysqli_real_escape_string($mysqli, $_POST['date']);
+      $time_SELECTED = mysqli_real_escape_string($mysqli, $_POST['time']);
+      $location_SELECTED = mysqli_real_escape_string($mysqli, $_POST['location']);
+      $type_SELECTED = mysqli_real_escape_string($mysqli, $_POST['type']);
+      $price_SELECTED = mysqli_real_escape_string($mysqli, $_POST['price']);
+      echo "event: ".$event_SELECTED." <br /> category: ".$category_SELECTED." <br /> date: ".$date_SELECTED.
+      "<br /> time: ".$time_SELECTED."<br /> location: ".$location_SELECTED.
+      "<br /> type: ".$type_SELECTED."<br />price: ".$price_SELECTED;
+      //echo $event_SELETED;
+
+      //$insertQuery1 = "INSERT INTO olympicEvent(name, 'date', 'time', location, type, category, ticketPrice) VALUES (test2, test2, 2016-08-05, 10:00:00, Arena Test, comp, 20.00)";
+      $insertQuery1 = "INSERT INTO olympicEvent(name, date, time, location, type, category, ticketPrice)"
+      ."VALUES ('$event_SELECTED', '$date_SELECTED', '$time_SELECTED', '$location_SELECTED',  '$type_SELECTED', '$category_SELECTED',  '$price_SELECTED')";
+
+      mysqli_query($mysqli, $insertQuery1);
+  }
+}
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #009900;">
@@ -26,71 +50,72 @@ if ($access != 'E') {
 </nav>
 
 <?php
-echo "ID: ".$_SESSION['first_name']." ".$_SESSION['last_name'];
-echo "<br />"
- ?>
+  echo "ID: ".$_SESSION['first_name']." ".$_SESSION['last_name'];
+  echo "<br />"
+?>
 
 <body>
 <input type="submit" onclick="toggle_visibility('tog');" value="Add to Schedule"/>
 <div id="tog" style="display:none;" >
 
 <div class = "EUI_scheduleContainer">
-<form class = "schedule" action="manageSchedule.php" method="post">
-  <h4>Add to Schedule</h4>
-    <div class = "grid4">
-    <span><strong>Event Name</strong></span>
-    <span><strong>Category</strong></span>
-    <span><strong>Date</strong></span>
-    <span><strong>Time</strong></span>
-    <span><strong>Location</strong></span>
-    <span><strong>Type</strong></span>
+  <form class = "schedule" action="manageSchedule.php" method="post">
+    <h4>Add to Schedule</h4>
+      <div class = "grid4">
+      <span><strong>Event Name</strong></span>
+      <span><strong>Category</strong></span>
+      <span><strong>Date</strong></span>
+      <span><strong>Time</strong></span>
+      <span><strong>Location</strong></span>
+      <span><strong>Type</strong></span>
+      <span><strong>Price</strong></span>
 
+      <!--Drop Down bars below-->
+      <select name="event">
+        <?php
+        $query = "SELECT name FROM eventlist";
+        $result = mysqli_query($mysqli, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<option value=".$row['name'].">".$row['name']."</option>";
+        }
+        ?>
+      </select>
+      <select name="category">
+        <?php
+        $query = "SELECT category FROM categorylist";
+        $result = mysqli_query($mysqli, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<option value=".$row['category'].">".$row['category']."</option>";
+        }
+        ?>
+      </select>
 
-    <select name="schedule">
-      <?php
-      $query = "SELECT name FROM eventlist";
-      $result = mysqli_query($mysqli, $query);
-      while ($row = mysqli_fetch_assoc($result)) {
-          echo "<option value=".$row['name'].">".$row['name']."</option>";
-      }
-      ?>
-    </select>
-    <select name="category">
-      <?php
-      $query = "SELECT category FROM categorylist";
-      $result = mysqli_query($mysqli, $query);
-      while ($row = mysqli_fetch_assoc($result)) {
-          echo "<option value=".$row['category'].">".$row['category']."</option>";
-      }
-      ?>
-    </select>
-<!--Drop Down bars below-->
-    <input type="text" name="date" placeholder="yyyy-mm-dd" required/>
-    <select name="time">
-      <option  value="10:00:00">10:00AM</option>
-      <option  value="10:30:00">10:30AM</option>
-      <option  value="11:00:00">11:30AM</option>
-      <option  value="11:30:00">11:30AM</option>
-      <option  value="12:00:00">12:00PM</option>
-      <option  value="12:00:00">12:00PM</option>
-      <option  value="12:00:00">12:00PM</option>
-      <option  value="12:00:00">12:00PM</option>
-      <option  value="12:00:00">12:00PM</option>
-    </select>
-    <input type="text" name="location" placeholder="Location" required/>
-    <select name="type">
-      <?php
-      $query = "SELECT type FROM typelist";
-      $result = mysqli_query($mysqli, $query);
-      while ($row = mysqli_fetch_assoc($result)) {
-          echo "<option value=".$row['type'].">".$row['type']."</option>";
-      }
-      ?>
-    </select>
-
-  </div>
-  <input type="submit"  name="event" />
-</form>
+      <input type="text" name="date" placeholder="yyyy-mm-dd" required/>
+      <select name="time">
+        <option  value="10:00:00">10:00AM</option>
+        <option  value="10:30:00">10:30AM</option>
+        <option  value="11:00:00">11:30AM</option>
+        <option  value="11:30:00">11:30AM</option>
+        <option  value="12:00:00">12:00PM</option>
+        <option  value="12:00:00">12:00PM</option>
+        <option  value="12:00:00">12:00PM</option>
+        <option  value="12:00:00">12:00PM</option>
+        <option  value="12:00:00">12:00PM</option>
+      </select>
+      <input type="text" name="location" placeholder="Location" required/>
+      <select name="type">
+        <?php
+        $query = "SELECT type FROM typelist";
+        $result = mysqli_query($mysqli, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<option value=".$row['type'].">".$row['type']."</option>";
+        }
+        ?>
+      </select>
+      <input type="text" name="price" placeholder="Price" required/>
+    </div>
+    <input type="submit"  name="AddtoSchedule_button" />
+  </form>
 </div>
 
 
