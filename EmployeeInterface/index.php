90a4f15d98a4dfa0dee2910fd2fe6b3c43101cb6
Thomson Kneeland <thomsonkneeland@gmail.com>
2019-04-10 17:53:19 -0400

    updated active links, header font colors, changed index names

--- EmployeeInterface/index.php
+++ EmployeeInterface/index.php
@@ -12,17 +12,18 @@ require 'employeeHeader.php';
 6) color scheme suggested only-->
 
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #009900;">
-  <a class="navbar-brand navbar-dark"><span class="h2"><p class="font-weight-bold">Summer Olympic Games</p></span></a>
+  <!-- <a class="navbar-brand navbar-dark"><span class="h2"><p class="font-weight-bold">Summer Olympic Games</p></span></a> -->
+  <a class="navbar-brand navbar-dark"><font color="white">Summer Olympic Games</font></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="employeeIndex.php">Dashboard<span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link active" href="addAthletes.php">Add Athletes</a>
-      <a class="nav-item nav-link active" href="modifySchedule.php">Modify Schedule</a>
-      <a class="nav-item nav-link active" href="modifyTickets.php">Modify Tickets</a>
-      <a class="nav-item nav-link active" href="logout.php"> Logout</a></li>
+      <a class="nav-item nav-link active" href="index.php">Dashboard<span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="addAthletes.php">Add Athletes</a>
+      <a class="nav-item nav-link" href="modifySchedule.php">Modify Schedule</a>
+      <a class="nav-item nav-link" href="modifyTickets.php">Modify Tickets</a>
+      <a class="nav-item nav-link" href="logout.php"> Logout</a></li>
     </div>
   </div>
 </nav>
