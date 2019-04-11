8fac11a128435efc412eb65e6c2a22c12f627209
Sammy Samkough <sammysamkough@gmail.com>
2019-04-10 20:31:34 -0400

    Updated athlete page

--- pages/athletePage.php
+++ pages/athletePage.php
@@ -3,7 +3,28 @@
 <html>
   <body>
     <?php require '../components/nav.php'; ?>
-    <p>test</p>
+    <div class="athletePage_container">
+      <hr>
+      <?php
+        //for now only name and country, could do sports later
+        $query1 = "SELECT * FROM athletes INNER JOIN users ON athletes.id=users.id";
+        echo "<table class=\"athletes\">";
+        $query1_result = mysqli_query($mysqli, $query1);
+        $counter = 1;
+        echo"<tr>";
+        while($row = mysqli_fetch_array($query1_result)) {
+          echo "<td><h4>" . $row['firstName'] . " " . $row['lastName'] . "</h3><ul><li>" . $row['country'] . "</li><li>" .
+          $row['DOB'] . "</li><li>" . $row['height'] . "</li><li>" . $row['wgt']  . "</ul></td>";
+          if($counter % 3 == 0) {
+           echo"</tr>";
+            echo"<tr>";
+          }
+          $counter++;
+        }
+        echo"</tr>";
+        echo "</table>";
+      ?>
+    </div>
     <?php require '../components/footer.php'; ?>
   </body>
 </html>
