b749329dc5003dc08c92aca1ad42a1dd994f9d62
Sammy Samkough <sammysamkough@gmail.com>
2019-04-26 01:44:24 -0400

    added form validation

--- PublicInterface/purchaseTickets.php
+++ PublicInterface/purchaseTickets.php
@@ -200,38 +200,58 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
                                         <!-- PAYMENT -->
                                         <h4 class="mb-3">Payment</h4>
                                         <div class="d-block my-3">
-                                        <div class="row">
-                                            <div class="col-md-6 mb-3">
-                                                <label for="cc-name">Name on card</label>
-                                                <input type="text" class="form-control" id="cc-name" placeholder="" required>
-                                                <small class="text-muted">Full name as displayed on card</small>
-                                                <div class="invalid-feedback">Name on card is required</div>
+                                            <div class="row">
+                                                <div class="col-md-6 mb-3">
+                                                    <label for="cc-name">Name on card</label>
+                                                    <input type="text" class="form-control" id="cc-name" required>
+                                                    <small class="text-muted">Full name as displayed on card</small>
+                                                    <div class="invalid-feedback">Name on card is required</div>
+                                                </div>
+
+                                                <div class="col-md-6 mb-3">
+                                                    <label for="cc-number">Credit card number</label>
+                                                    <input type="text" class="form-control" id="cc-number" placeholder="" required>
+                                                    <div class="invalid-feedback">Credit card number is required</div>
+                                                </div>
                                             </div>
 
-                                            <div class="col-md-6 mb-3">
-                                                <label for="cc-number">Credit card number</label>
-                                                <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                                                <div class="invalid-feedback">Credit card number is required</div>
+                                            <div class="row">
+                                                <div class="col-md-3 mb-3">
+                                                    <label for="cc-expiration">Expiration</label>
+                                                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+                                                    <div class="invalid-feedback">Expiration date required</div>
+                                                </div>
+
+                                                <div class="col-md-3 mb-3">
+                                                    <label for="cc-cvv">CVV</label>
+                                                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+                                                    <div class="invalid-feedback">Security code required</div>
+                                                </div>
                                             </div>
                                         </div>
-
-                                        <div class="row">
-                                            <div class="col-md-3 mb-3">
-                                                <label for="cc-expiration">Expiration</label>
-                                                <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                                                <div class="invalid-feedback">Expiration date required</div>
-                                            </div>
-
-                                            <div class="col-md-3 mb-3">
-                                                <label for="cc-cvv">CVV</label>
-                                                <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                                                <div class="invalid-feedback">Security code required</div>
-                                            </div>
-                                        </div>
-
                                         <hr class="mb-4">
                                         <button id="confirmTicketPurchaseBtn" class="btn btn-primary btn-lg btn-block" name="confirmPurchaseBtn"></button>
                                     </form>
+                                    <script>
+                                    // Example starter JavaScript for disabling form submissions if there are invalid fields
+                                    (function() {
+                                        'use strict';
+                                        window.addEventListener('load', function() {
+                                            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                                            var forms = document.getElementsByClassName('needs-validation');
+                                            // Loop over them and prevent submission
+                                            var validation = Array.prototype.filter.call(forms, function(form) {
+                                                form.addEventListener('submit', function(event) {
+                                                    if (form.checkValidity() === false) {
+                                                        event.preventDefault();
+                                                        event.stopPropagation();
+                                                    }  
+                                                    form.classList.add('was-validated');
+                                                    }, false);
+                                                });
+                                            }, false);
+                                        })();
+                                </script>
                                 </div>
                             </div>
                         </div>
