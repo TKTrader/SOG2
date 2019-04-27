850674f1f8b40f5d4b141d1889ea6d1c64f52a26
AlexEspo <45057180+AlexEspo@users.noreply.github.com>
2019-04-27 13:53:39 -0400

    Add files via upload

--- AthleteInterface/notificationPage.php
+++ AthleteInterface/notificationPage.php
@@ -27,7 +27,7 @@ if ($access != 'A') {
   </span>
   <div class="bell">
     <a class="navbar-brand float-right" href="notificationPage.php">
-          <img src="bell.png" height="30" alt="">
+          <img src="../assets/bell.png" height="30" alt="">
         </a>
 </div>
 </nav>
