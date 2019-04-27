591a1da71ff028f60f52c0fab30f1c80dd101498
AlexEspo <45057180+AlexEspo@users.noreply.github.com>
2019-04-27 12:16:51 -0400

    Add files via upload

--- eventsPage.php
+++ eventsPage.php
@@ -3,25 +3,43 @@
   $mysqli->set_charset("utf8");
 ?>
 
-  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand">Summer Olympic Games</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link active" href="eventsPage.php">Events</a>
-        <a class="nav-item nav-link" href="schedulePage.php">Schedule</a>
-        <a class="nav-item nav-link" href="athletePage.php">Athletes</a>
-        <a class="nav-item nav-link" href="registerPage.php">Register</a>
-        <a class="nav-item nav-link" href="loginPage.php">Login</a>
-      </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+  <a class="navbar-brand" href="index.php">Summer Olympic Games</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="athletePage.php">Athletes</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Event Details
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item active" href="eventsPage.php">Events</a>
+          <a class="dropdown-item" href="schedulePage.php">Schedule</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="registerPage.php">Register</a>
+            <a class="dropdown-item" href="loginPage.php">Login</a>
+        </div>
+      </li>
     </div>
+</div>
+
+<div class="bell">
     <a class="navbar-brand float-right" href="notificationPage.php">
-      <img class="img-responsive" width="70px" height="40px" src="assets/rio-2016-logo.png">
-    </a>
-  </nav>
+          <img src="bell.png" height="30" alt="">
+        </a>
+</div>
+</nav>
 
   <div class="grid_container">
     <div class="grid">
